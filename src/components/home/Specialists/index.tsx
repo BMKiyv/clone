d8c3c2932e98babf22a8cb8a6fcebe2204c1d997
Павел Дикий <yourproekt@gmail.com>
2021-06-16 01:44:10 +0300

    fixed bleat

--- src/components/home/Specialists/index.tsx
+++ src/components/home/Specialists/index.tsx
@@ -7,7 +7,7 @@ import { specialistsI } from "@/interfaces/home";
 import './style.scss';
 
 //Components
-//import Btn from "@/components/Btn";
+import Btn from "@/components/Btn";
 import { specialists } from '@/jsons';
 
 //Utils
@@ -29,17 +29,10 @@ const Specialist:React.FC  = () => {
 
         let countElements = 0;
 
-        let width = window.innerWidth;
-
         for (const item of specialists) {
-            //console.log("-----", countArrays, countElements, width);
-            if (countElements < 3 && width > 1023) {
+            //console.log("-----", countArrays, countElements);
+            if (countElements < 4) {
                 countElements += 1;
-            } else if (countElements <= 1 && width < 1024) {
-                groupArrSpec[0] = [];
-                countElements = 1;
-                countArrays += 1;
-                groupArrSpec[countArrays] = [];
             } else {
                 countArrays += 1;
                 countElements = 1;
@@ -95,14 +88,103 @@ const Specialist:React.FC  = () => {
                 <div>
                     <p className = 'specialists__description'>{I18n.t('specialistsDescr')}</p>
                 </div>
+                {/*<div className = 'specialists__items'>
+
+                    <div className = 'specialists__item'>
+                        <figure className = 'specialists__figure'>
+                            <div className = 'specialists__img-wrap specialists__img--spatak_subbota'>
+                                 <img src="/img/specialists/spatak_subbota.png" alt="Спартак Суббота"
+                                     className="specialists__img"/>
+                            </div>
+                            <figcaption className = 'specialists__author'>Спартак Суббота</figcaption>
+                        </figure>
+
+                        <div className = 'specialists__text-wrap'>
+                            <p className = 'specialists__text specialists__text--mobile'>
+                                Науковий керівник Інституту когнітивного моделювання
+                            </p>
+                            <p className = 'specialists__text specialists__text--desktop'>
+                                Науковий керівник Інституту когнітивного моделювання, кандидат психологічних наук,
+                                лікар-психіатр, практикуючий психолог у методі когнітивно-поведінкової терапії з
+                                п'ятирічним досвідом роботи.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className = 'specialists__item'>
+                        <figure className = 'specialists__figure'>
+                            <div className = 'specialists__img-wrap specialists__img--sofia_lagutina'>
+                                <img src="/img/specialists/sofia_lagutina.png" alt="Софія Лагутіна"
+                                     className="specialists__img"/>
+                            </div>
+                            <figcaption className = 'specialists__author'>Софія Лагутіна</figcaption>
+                        </figure>
+                        <div className = 'specialists__text-wrap'>
+                            <p className = 'specialists__text specialists__text--mobile'>
+                                Провідний спеціаліст Інституту когнітивного моделювання, лікар-психолог, аспірант
+                            </p>
+                            <p className = 'specialists__text specialists__text--desktop'>
+                                Провідний спеціаліст Інституту когнітивного моделювання, лікар-психолог, аспірант
+                                Національного медичного університету ім. А.А. Богомольця.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className = 'specialists__item'>
+                        <figure className = 'specialists__figure'>
+                            <div className = 'specialists__img-wrap specialists__img--lesia_sak'>
+                                 <img src="/img/specialists/lesia_sak.png" alt="Леся Сак" className="specialists__img"/>
+                            </div>
+                            <figcaption className = 'specialists__author'>Леся Сак</figcaption>
+                        </figure>
+                        <div className = 'specialists__text-wrap'>
+                            <p className = 'specialists__text specialists__text--mobile'>
+                                Лікар - психолог, психотерапевт, асистент кафедри медичної психології
+                            </p>
+                            <p className = 'specialists__text specialists__text--desktop'>
+                                Лікар- психолог, психотерапевт, асистент кафедри медичної психології, психосоматичної
+                                медицини та психотерапії Національного медичного університету ім. О.О. Богомольця.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className = 'specialists__item'>
+                        <figure className = 'specialists__figure'>
+                            <div className = 'specialists__img-wrap specialists__img--bogdana_valigu'>
+                                <img src="/img/specialists/bogdana_valigura.png" alt="Богдана Валігура"
+                                     className="specialists__img"/>
+                            </div>
+                            <figcaption className = 'specialists__author'>Богдана Валігура</figcaption>
+                        </figure>
+                        <div className = 'specialists__text-wrap'>
+                            <p className = 'specialists__text specialists__text--mobile'>
+                                Психолог, психотерапевт з пятирічним досвідом роботи.
+                            </p>
+                            <p className = 'specialists__text specialists__text--desktop'>
+                                Психолог, психотерапевт з пятирічним досвідом роботи. Спеціалізується на работі з
+                                панічніми атаками, з депресією, тривожним розладом, соціальною тривогою та особистісними
+                                конфліктам. Сертифікований коуч.
+                            </p>
+                        </div>
+                    </div>
+                </div>*/ }
+
                 <Carousel
                     { ...carouselPropSpecialists() }>
                     { renderSpecialist() }
                 </Carousel>
+
+                <div className = 'btn_wrap specialists_wrap-btn'>
+                    <Btn
+                        theme = 'main'
+                        title = { I18n.t('specialistsBtn') }
+                        url = '/get-help'
+                    />
+                </div>
             </div>
         </section>
     )
-    ;
+        ;
 };
 
 export default Specialist;
