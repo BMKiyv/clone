903d2a02264388535fff35091941ecb938b35dc6
Michael <k1brusenets@gmail.com>
2021-06-15 22:06:21 +0300

    fixing styles +btnMore

--- src/components/home/About/index.tsx
+++ src/components/home/About/index.tsx
@@ -5,6 +5,7 @@ import I18n from "@/I18n";
 
 //Components
 import Btn from "@/components/Btn";
+import BtnMore from '@/components/BtnMore';
 
 const About:React.FC = () => {
 
@@ -18,37 +19,45 @@ const About:React.FC = () => {
         <section className = 'about'>
             <div className='content-wrap'>
             <h2 className = 'title about-title'>{I18n.t('about')}</h2>
-            <div className = 'anchor' id = 'about' />
-            <div className = 'about-wrap'>
-                <div className = 'about__content pad'>
-                    <div className = { `about_description ${isOpen ? "about_description--full" : ""}` }>
-                        <p>
-                            {I18n.t('homeAboutText1')}
-                        </p>
-                        <img alt = 'partner1' src = '/img/inf-about.svg' />
+                    <div className = 'anchor' id = 'about' />
+                    <div className = 'about-wrap'>                   
+                        <div className = 'about__content pad'>
+                            <div className = 'about_description'>
+                                <p>
+                                    {I18n.t('homeAboutText1')}
+                                </p>
+                                <img alt = 'partner1' src = '/img/inf-about.svg' />
+                            </div>
+                        </div>
+                        <div className = 'infographic about__infographic'>
+                        <div className = {`about__height ${isOpen ? 'about__height-more': ''}`}>
+                            <p>
+                                {I18n.t('homeAboutText2')}
+                            </p>
+                            <p>
+                                {I18n.t('homeAboutText3')}
+                            </p>
+                            <p>
+                                {I18n.t('homeAboutText4')}
+                            </p>
+                            </div>
+                            <BtnMore
+                            title= {isOpen? 'Згорнути': 'Дізнатися більше'}
+                            onPress = {toggleOpen}
+                            
+                            />
+                            <div className = { `about_btn-more ${isOpen ? "about_btn-more--full" : ""}` }>
+                                <Btn
+                                    full
+                                    theme = 'main'
+                                    title = { 'Отримати психологічну допомогу' }
+                                    url = { 'https://itseems.com.ua' }
+                                    //onPress = { toggleOpen }
+                                />
+                            </div>
+
                     </div>
                 </div>
-                <div className = 'infographic about__infographic'>
-                    <p>
-                        {I18n.t('homeAboutText2')}
-                    </p>
-                    <p>
-                        {I18n.t('homeAboutText3')}
-                    </p>
-                    <p>
-                        {I18n.t('homeAboutText4')}
-                    </p>
-                    <div className = { `about_btn-more ${isOpen ? "about_btn-more--full" : ""}` }>
-                        <Btn
-                            full
-                            theme = 'main'
-                            title = { 'Отримати психологічну допомогу' }
-                            url = { 'https://itseems.com.ua' }
-                            onPress = { toggleOpen }
-                        />
-                    </div>
-                </div>
-            </div>
             </div>
         </section>
     );
